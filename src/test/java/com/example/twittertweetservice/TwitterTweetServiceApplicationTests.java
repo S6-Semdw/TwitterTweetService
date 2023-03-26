a00d5d1469d8f005149ee42f64dca537e31b4ea6
Sem de Wilde <sem.dewilde6@gmail.com>
2023-03-26 22:24:09 +0200

    Initial commit

---
+++ src/test/java/com/example/twittertweetservice/TwitterTweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twittertweetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterTweetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
